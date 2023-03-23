bfd96c4228d031292e7fb316bdde48c42996e880
schirica7 <91565444+schirica7@users.noreply.github.com>
2023-03-23 15:33:41 -0400

    Starting game/background

--- CountryballDrop/GameScene.swift
+++ CountryballDrop/GameScene.swift
@@ -11,10 +11,21 @@ import GameplayKit
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var maxHeight = 0.0
+    var warningHeight = 0.0
+    
+    //To lose: if the ball's y + ball.height/2 >= max height
+    //Warning: if the ball's y + ball.height/2 >= warning height
     
     override func didMove(to view: SKView) {
+        maxHeight = self.size.height * 0.9
+        warningHeight = self.size.height * 0.7
         
+        backgroundColor = UIColor(red: 158/255, green: 217/255, blue: 218/255, alpha: 1)
+        
+        let bottom = SKSpriteNode(color: UIColor(red: 255/255, green: 210/255, blue: 79/255, alpha: 1), size: CGSize(width: self.size.width, height: self.size.height * 0.11))
+        bottom.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.055)
+        addChild(bottom)
     }
     
     
